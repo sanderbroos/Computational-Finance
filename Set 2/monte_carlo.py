46fa173c6607c2587d6ec2fb1115b4233f81963e
Nick van Santen <vansantennick@gmail.com>
2022-03-02 16:20:19 +0100

    Implemented asian option value for monte carlo method

--- Set 2/monte_carlo.py
+++ Set 2/monte_carlo.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.stats import gmean
 
 class MonteCarloStock():
     """
@@ -12,14 +12,18 @@ class MonteCarloStock():
         self.K = K
         self.r = r
         self.S0 = S
+        self.S = S
         self.vol = vol
         self.option_type = option_type
         self.pricing_type = pricing_type
 
-    def calc_stock_price(self):
+    def calc_stock_price(self, tau=None):
+
+        if not tau:
+            tau = self.T
 
         # Equation 2
-        return self.S0 * np.exp((self.r - 0.5 * self.vol ** 2) * self.T + self.vol * np.sqrt(self.T) * np.random.normal(0, 1))
+        return self.S * np.exp((self.r - 0.5 * self.vol ** 2) * tau + self.vol * np.sqrt(tau) * np.random.normal(0, 1))
 
     def calc_payoff(self):
 
@@ -29,8 +33,23 @@ class MonteCarloStock():
             return max(self.calc_stock_price() - self.K, 0)
         elif self.option_type == "digital":
             return 1 if self.calc_stock_price() > self.K else 0
+        elif self.option_type == "asian":
+            return self.calc_asian_payoff()
         else:
             raise Exception(f"Error: invalid option type {self.option_type}")
+
+    def calc_asian_payoff(self):
+
+        N = 100
+        dt = self.T / N
+        stock_values = [self.S0]
+        for i in range(N):
+
+            self.S = self.calc_stock_price(tau = dt)
+
+            stock_values.append(self.S)
+
+        return max(0, gmean(stock_values) - self.K)
 
 class MonteCarloStockManager():
     """
@@ -74,9 +93,9 @@ class MonteCarloStockManager():
 
 def main():
 
-    manager = MonteCarloStockManager(1000000, option_type="digital")
+    manager = MonteCarloStockManager(100000, option_type="asian")
 
-    print(manager.calc_hedge_parameter(epsilon=0.01, fixed_seed=True))
+    print(manager.calc_option_price())
 
 if __name__ == "__main__":
     main()
